663ee149d1fe65887bdd78a434772b8cadeb88d8
YuAhnn <tcc.hermoso.y@gmail.com>
2026-02-14 12:39:58 +0800

    chore: 1st^2 commit -_-

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="flex justify-center items-center">
-            <h1 className="text-4xl font-bold">Hello World</h1>
+            <h1 className="text-4xl font-bold">Hello World 😎</h1>
         </div>
     );
 }
